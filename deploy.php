7676bd5c244171bd1d39799118d7d89cd3d9da97
Sander De la Marche <sander@sumocoders.be>
2020-12-01 18:23:49 +0100

    Update deploy

--- deploy.php
+++ deploy.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Deployer;
 
 require 'recipe/symfony.php';
@@ -6,28 +7,37 @@ require 'recipe/symfony.php';
 // Project name
 set('application', 'vrtlite');
 
-set('user', 'root');
-
 // Project repository
 set('repository', 'git@github.com:dreadnip/vrtlite.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
-set('git_tty', true); 
+set('git_tty', true);
+
+set('composer_options', 'install --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
+
+set('keep_releases', 3);
 
 // Shared files/dirs between deploys 
-add('shared_files', []);
+add('shared_files', ['.env']);
 add('shared_dirs', []);
 
 // Writable dirs by web server 
 add('writable_dirs', []);
 
-
 // Hosts
-
 host('vrtlite.be')
-    ->set('deploy_path', '~/var/www/html');
-    
+    ->hostname('vrtlite.be')
+    ->user('sander')
+    ->port(22)
+    ->set('branch', 'master')
+    ->set('deploy_path', '~/var/www/vrtlite')
+;
 // Tasks
+
+task('pwd', function () {
+    $result = run('pwd');
+    writeln("Current dir: $result");
+});
 
 task('build', function () {
     run('cd {{release_path}} && build');
@@ -35,8 +45,3 @@ task('build', function () {
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
-
-// Migrate database before symlink new release.
-
-before('deploy:symlink', 'database:migrate');
-
