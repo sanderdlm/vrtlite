8cdd46333494a2f56dd656a80a5667b2f4d6e870
Sander De la Marche <sander@sumocoders.be>
2020-12-01 16:19:11 +0100

    Add info/about page

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AboutController extends AbstractController
+{
+    /**
+     * @Route("/about", name="about")
+     */
+    public function __invoke()
+    {
+        return $this->render('about.html.twig',);
+    }
+}
