ca3f2633584ac37913e7a3dbf8cb065cdea466a9
Sander De la Marche <sander@sumocoders.be>
2023-03-14 13:27:50 +0100

    Finalize Deployer 7 deploy configuration

--- deploy.php
+++ deploy.php
@@ -6,7 +6,6 @@ require 'recipe/symfony.php';
 
 set('application', 'vrtlite');
 set('repository', 'git@github.com:dreadnip/vrtlite.git');
-set('composer_options', 'install --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
 set('keep_releases', 3);
 
 add('shared_files', ['.env']);
@@ -22,4 +21,6 @@ after('deploy:failed', 'deploy:unlock');
 
 task('clear:opcache', function () {
     run('{{bin/php}} {{ release_or_current_path }}/vendor/bin/chop');
-})->addAfter('deploy:symlink');
+});
+
+after('deploy:symlink', 'clear:opcache');
