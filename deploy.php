75033c47661da829487b8c2e5ad54d2ce3f60a10
Sander De la Marche <sander@sumocoders.be>
2023-03-14 13:07:45 +0100

    Bump to 8.2

--- deploy.php
+++ deploy.php
@@ -4,25 +4,17 @@ namespace Deployer;
 
 require 'recipe/symfony.php';
 
-// Project name
 set('application', 'vrtlite');
-
-// Project repository
 set('repository', 'git@github.com:dreadnip/vrtlite.git');
-
 set('composer_options', 'install --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader');
-
 set('keep_releases', 3);
 
-// Shared files/dirs between deploys 
 add('shared_files', ['.env']);
 add('shared_dirs', []);
 
-// Hosts
 host('vrtlite.be')
-    ->hostname('vrtlite.be')
-    ->user('sander')
-    ->port(22)
+    ->setRemoteUser('sander')
+    ->setPort(22)
     ->set('branch', 'master')
     ->set('deploy_path', '/var/www/vrtlite');
 
